255262dea9099b6c13a3bd894764574c55f7fa74
Maurice Noel Bouniol <mxb1010@cs.bham.ac.uk>
2018-08-10 21:06:17 +0100

    wipe

--- client/src/index.js
+++ client/src/index.js
@@ -1,66 +1,8 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
-
-import React, { Component } from 'react';
-import Konva from 'konva';
-import { render } from 'react-dom';
-import { Stage, Layer, Star, Text } from 'react-konva';
-
-class App extends Component {
-  handleDragStart = e => {
-    e.target.setAttrs({
-      shadowOffset: {
-        x: 15,
-        y: 15
-      },
-      scaleX: 1.1,
-      scaleY: 1.1
-    });
-  };
-  handleDragEnd = e => {
-    e.target.to({
-      duration: 0.5,
-      easing: Konva.Easings.ElasticEaseOut,
-      scaleX: 1,
-      scaleY: 1,
-      shadowOffsetX: 5,
-      shadowOffsetY: 5
-    });
-  };
-  render() {
-    return (
-      <Stage width={window.innerWidth} height={window.innerHeight}>
-        <Layer>
-          <Text text="Try to drag a star" />
-          {[...Array(10)].map(i => (
-            <Star
-              key={i}
-              x={Math.random() * window.innerWidth}
-              y={Math.random() * window.innerHeight}
-              numPoints={5}
-              innerRadius={20}
-              outerRadius={40}
-              fill="#89b717"
-              opacity={0.8}
-              draggable
-              rotation={Math.random() * 180}
-              shadowColor="black"
-              shadowBlur={10}
-              shadowOpacity={0.6}
-              onDragStart={this.handleDragStart}
-              onDragEnd={this.handleDragEnd}
-            />
-          ))}
-        </Layer>
-      </Stage>
-    );
-  }
-}
-
-render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://databaseadmin:databaseadmin1@ds245661.mlab.com:45661/big-dev'
+};
