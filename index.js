cb954169be2eace386520a87aee8dff1ef8c480a
Maurice Noel Bouniol <mnb.dev.in@gmail.com>
2018-08-14 17:16:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ mongoose.connect(keys.mongoURI);  //****** THE DATABASE HAS TO BE REBUILD ******
 // Running Express app, all Route Handlers are registed with that 'app' object
 const app = express();
 
-
-
 app.use(
   cookieSession({
       maxAge: 30 * 24 * 60 * 60 * 1000, // how long can this cookie exist in the browser before it expires, Days * Hours * Minutes * Seconds * Miliseconds
@@ -42,6 +40,20 @@ require('./routes/authRoutes')(app);
 // const route = '/'
 // app.get(route,handler);
 
+// When a request comes into express, it will first check if there is a specific file that matches the request, if the file exists, the server answers with assets, if not the serer continues to the 'catch all' route
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file
+  app.use(express.static('client/build')); // if a request comes in for some route/file/anything and the server does not understand what to do, then look into the client/build to see if there is a static main.js
+
+  // Works as a 'catch all' in the app, if the first answer fails to fulfil the request
+  // Express will serve up the index.html file
+  // if it doesnt recognise the route
+  const path = require('path');
+  app.get('*', (req, res) => { // if a request comes in for some route and the server does understand what to do, it serves the html document, so it is assumed that the react-route is responsible for this route
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // A port that should not be changed, Heroku will listen to this port
 // The eviornment variables are given her, which are environment variables of the runtime the node is running on
